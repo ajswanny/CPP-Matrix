ad47e6f4f302be5a2c2efc67baeb4a16c717df8e
Alexander Swanson <alexander.swanson.1@uvm.edu>
2018-07-12 13:59:30 -0400

    Committing project files.

---
+++ cmake-build-debug/CMakeFiles/CPP_Matrix.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CPP_Matrix.dir/main.cpp.o"
+  "CPP_Matrix.pdb"
+  "CPP_Matrix"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CPP_Matrix.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
